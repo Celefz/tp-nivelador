2c78a4b48dbf34229f2bd11c58d3b80b99b55a6f
Celefz <fzeolla@fi.uba.ar>
2026-09-10 03:50:46 -0300

    Ejercicio 5: Serialización y deserialización para servidor

---
+++ services/server/src_frozen/protocol/protocol.py
@@ -0,0 +1,78 @@
+import struct
+from services.server.src_frozen.lottery.bet import Bet
+
+TYPE_BET = 1
+
+HEADER_LEN = 4
+MIN_PACKET_LEN = 20
+
+
+def serialize(bet) -> bytes:
+    first_name = bet.first_name.encode("utf-8")
+    last_name = bet.last_name.encode("utf-8")
+    birthdate = bet.birthdate.encode("utf-8")
+
+    first_name_len = len(first_name)
+    last_name_len = len(last_name)
+
+    bet_data = bytearray()
+
+    bet_data.append(TYPE_BET)
+    bet_data.append(bet.agency_id)
+    bet_data.append(first_name_len)
+    bet_data.append(last_name_len)
+
+    bet_data.extend(first_name)
+    bet_data.extend(last_name)
+    bet_data.extend(birthdate)
+    bet_data.extend(bet.document.to_bytes(4, byteorder="big"))
+    bet_data.extend(bet.number.to_bytes(2, byteorder="big"))
+
+    return bytes(bet_data)
+
+
+def deserialize(bet_data: bytes):
+    if len(bet_data) < HEADER_LEN:
+        raise ValueError("packet too short")
+
+    if bet_data[0] != TYPE_BET:
+        raise ValueError(f"invalid bet type: {bet_data[0]}")
+
+    agency_id = bet_data[1]
+    first_name_len = bet_data[2]
+    last_name_len = bet_data[3]
+
+    if len(bet_data) != MIN_PACKET_LEN + first_name_len + last_name_len:
+        raise ValueError("invalid bet data: wrong size")
+
+    first_name_start = 4
+    birthdate_len = 10
+    document_len = 4
+
+    last_name_start = first_name_start + first_name_len
+    birthdate_start = last_name_start + last_name_len
+    document_start = birthdate_start + birthdate_len
+    number_start = document_start + document_len
+
+    first_name = bet_data[first_name_start:last_name_start].decode("utf-8")
+    last_name = bet_data[last_name_start:birthdate_start].decode("utf-8")
+    birthdate = bet_data[birthdate_start:document_start].decode("utf-8")
+
+    document = int.from_bytes(
+        bet_data[document_start:number_start],
+        byteorder="big"
+    )
+
+    number = int.from_bytes(
+        bet_data[number_start:],
+        byteorder="big"
+    )
+
+    return Bet(
+        agency_id=agency_id,
+        first_name=first_name,
+        last_name=last_name,
+        document=document,
+        birthdate=birthdate,
+        number=number,
+    )
