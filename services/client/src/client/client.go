f18406bfa0bfb2dc2aa991dea3006bb78b8fc529
Celefz <fzeolla@fi.uba.ar>
2026-09-10 14:29:15 -0300

    Refactor de cliente

--- services/client/src/client/client.go
+++ services/client/src/client/client.go
@@ -17,7 +17,7 @@ import (
 )
 
 const CONNECTION_ATTEMPTS_MAX = 3
-const CONNECTION_ATTEMPS_DELAY_MS = 200
+const CONNECTION_ATTEMPS_DELAY_MS = 1000
 
 type ClientConfig struct {
 	ServerHost string
@@ -97,48 +97,82 @@ func (client *Client) Run() error {
 	defer client.conn.Close()
 
 	clientArgs := []any{"agency-id", client.config.AgencyId}
-	agencyID, err := strconv.ParseUint(client.config.AgencyId, 10, 8)
+
+	agencyID, err := client.parseAgencyID()
 	if err != nil {
-		return fmt.Errorf("invalid agency id %q: %w", client.config.AgencyId, err)
+		return err
 	}
 
-	inputFileName := client.config.InputFile
-	inputFile, err := os.Open(inputFileName)
-
+	inputFile, err := os.Open(client.config.InputFile)
 	if err != nil {
 		logger.Error("open-input-file", logger.Fail, clientArgs...)
 		return err
 	}
 	defer inputFile.Close()
 
-	batchSize, err := strconv.ParseInt(client.config.BatchSize, 10, 16)
+	batchSize, err := client.parseBatchSize()
 
-	if err != nil || batchSize == 0 {
-		return fmt.Errorf("invalid batch size %q", client.config.BatchSize)
+	if err != nil {
+		return err
 	}
 
-	batch := make([]lottery.Bet, 0, int(batchSize))
+	if err := client.sendBets(inputFile, agencyID, batchSize, clientArgs); err != nil {
+		return err
+	}
 
+	outputFile, err := os.Create(client.config.OutputFile)
+
+	if err != nil {
+		logger.Error("create-output-file", logger.Fail, clientArgs...)
+		return err
+	}
+
+	defer outputFile.Close()
+
+	if err := client.recvWinners(outputFile, clientArgs); err != nil {
+		return err
+	}
+
+	logger.Info(mainAction, logger.Success, clientArgs...)
+	return nil
+}
+
+func (client *Client) parseAgencyID() (uint8, error) {
+	agencyID, err := strconv.ParseUint(client.config.AgencyId, 10, 8)
+	if err != nil {
+		return 0, fmt.Errorf("invalid agency id %q: %w", client.config.AgencyId, err)
+	}
+
+	return uint8(agencyID), nil
+}
+
+func (client *Client) parseBatchSize() (int, error) {
+	batchSize, err := strconv.ParseInt(client.config.BatchSize, 10, 16)
+	if err != nil || batchSize == 0 {
+		return 0, fmt.Errorf("invalid batch size %q", client.config.BatchSize)
+	}
+
+	return int(batchSize), nil
+}
+
+func (client *Client) sendBets(inputFile io.Reader, agencyID uint8, batchSize int, clientArgs []any) error {
+	batch := make([]lottery.Bet, 0, batchSize)
 	scanner := bufio.NewScanner(inputFile)
 
 	for scanner.Scan() {
-		logger.Info(mainAction, logger.InProgress, clientArgs...)
-		bet, err := lottery.ParseBetFromCsv(scanner.Text(), uint8(agencyID))
+		logger.Info("send-bets", logger.InProgress, clientArgs...)
 
+		bet, err := lottery.ParseBetFromCsv(scanner.Text(), agencyID)
 		if err != nil {
 			logger.Error("parse-bet", logger.Fail, clientArgs...)
 			return err
 		}
 
 		batch = append(batch, bet)
-
-		if len(batch) == int(batchSize) {
-			packet := protocol.SerializeBatch(batch, uint8(agencyID))
-			if err := safe_socket.SendAll(client.conn, packet); err != nil {
-				logger.Error("send-batch", logger.Fail, clientArgs...)
+		if len(batch) == batchSize {
+			if err := client.sendBatch(batch, agencyID, clientArgs); err != nil {
 				return err
 			}
-
 			batch = batch[:0]
 		}
 	}
@@ -147,28 +181,40 @@ func (client *Client) Run() error {
 		logger.Error("scan-input-file", logger.Fail, clientArgs...)
 		return err
 	}
+
 	if len(batch) > 0 {
-		packet := protocol.SerializeBatch(batch, uint8(agencyID))
-		if err := safe_socket.SendAll(client.conn, packet); err != nil {
-			logger.Error("send-batch", logger.Fail, clientArgs...)
+		if err := client.sendBatch(batch, agencyID, clientArgs); err != nil {
 			return err
 		}
 	}
 
-	if err := safe_socket.SendAll(client.conn, protocol.SerializeEnd(uint8(agencyID))); err != nil {
+	if err := client.sendEnd(agencyID, clientArgs); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (client *Client) sendBatch(batch []lottery.Bet, agencyID uint8, clientArgs []any) error {
+	packet := protocol.SerializeBatch(batch, agencyID)
+	if err := safe_socket.SendAll(client.conn, packet); err != nil {
+		logger.Error("send-batch", logger.Fail, clientArgs...)
+		return err
+	}
+
+	return nil
+}
+
+func (client *Client) sendEnd(agencyID uint8, clientArgs []any) error {
+	if err := safe_socket.SendAll(client.conn, protocol.SerializeEnd(agencyID)); err != nil {
 		logger.Error("send-end", logger.Fail, clientArgs...)
 		return err
 	}
 
-	outputFileName := client.config.OutputFile
-	outputFile, err := os.Create(outputFileName)
+	return nil
+}
 
-	if err != nil {
-		logger.Error("create-output-file", logger.Fail, clientArgs...)
-		return err
-	}
-	defer outputFile.Close()
-
+func (client *Client) recvWinners(outputFile io.Writer, clientArgs []any) error {
 	for {
 		packet, err := recvPacket(client.conn)
 		if err != nil {
@@ -177,11 +223,10 @@ func (client *Client) Run() error {
 		}
 
 		if protocol.IsEndPacket(packet) {
-			break
+			return nil
 		}
 
 		winners, err := protocol.DeserializeBatch(packet)
-
 		if err != nil {
 			logger.Error("deserialize-winner", logger.Fail, clientArgs...)
 			return err
@@ -194,8 +239,4 @@ func (client *Client) Run() error {
 			}
 		}
 	}
-
-	logger.Info(mainAction, logger.Success, clientArgs...)
-
-	return nil
 }
